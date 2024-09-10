98e58c792c0f129490776c0fd48ba820ec2add25
Abdullah <abdullah.iqqbal487@gmail.com>
2024-09-10 14:24:30 +0500

    intial commit

---
+++ js-refresher/script.js
@@ -0,0 +1,82 @@
+// string in javascript
+
+// "use strict";
+// let a = 34;
+// let a = 'This is me';
+// console.log(a);
+// let a = 45;
+// a = 4554 + 4;
+// console.log(a);
+// let name1 = "Harry";
+// console.log(name1[0]);
+// console.log(name1[1]);
+// console.log(name1);
+
+//function hello() {
+// console.log("hey harry");
+//}
+//let abdullah = {
+//r: 34,
+// m: 64,
+// func: function myfunc(number) {
+// console.log("The number is" + number);
+//},
+//};
+//document.addEventListener("click", function click() {
+// console.log("clicked");
+// alert("hello")
+// let conf = confirm("Are you sure?");
+// console.log(conf);
+//});
+// for (let index =0; index < array.length; index++) {
+// const element = array[index];
+//}
+//let arr = [34, 21, 23];
+//function myfunc(number) {
+// console.log("The number is " + number);
+//}
+// =================================================
+
+// Variables, Data Types and Operators in JavaScript
+
+// var a = 78;
+// var b = "Abdullah";
+// c = 34.55;
+// console.log(c);
+
+//operators in javascript
+// operand - entities on which operators operate
+// in 3 + 4 '+' is the operator and 3,4 are operands
+// 1. unary operator - It has single operand (x = -x)
+// Examples of unary operator
+// c = -c;
+// console.log(c)
+// 2. binary operator - Tt has two perand (x = -x+6)
+// Examples of binary operator
+// c = 456 + 8;
+// console.log(c);
+
+// var num1 = 79;
+// var num2 = 67;
+
+// Arithmetic operators in action in JavaScript
+// console.log("The value of num1 + num2 is " + (num1 + num2));
+// console.log("The value of num1 - num2 is " + (num1 - num2));
+// console.log("The value of num1 * num2 is " + num1 * num2);
+// console.log("The value of num1 / num2 is " + num1 / num2);
+// console.log("The value of num1 ** num2 is " + num1 ** num2);
+// console.log("The value of num1++ num2 is " + num1++);
+// console.log("The value of ++num1 num2 is " + ++num1);
+// console.log("The value of num1-- num2 is " + num1--);
+// console.log("The value of --num1 num2 is " + --num1);
+
+// javascript | strings and string methods
+
+// var string = 'this';
+//   var string = 'thi"s';
+//   var name = 'harry';
+//   var chennel = 'CodeWithHarry';
+//   var message = 'Abdullah is a good boy'
+//   var temp = `${name} is a nice person and he has a chennel called ${chennel}`;
+// console.log(string + name + message);
+//   console.log(temp)
